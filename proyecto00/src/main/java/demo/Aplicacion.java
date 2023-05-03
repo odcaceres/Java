fde4b3e15de29780a7df501c6a5ff41f2fd51cd5
oscar caceres <oscarcaceres.one@gmail.com>
2023-05-02 21:39:26 -0300

    Primer archivo creado Aplicacion.java

---
+++ proyecto00/src/main/java/demo/Aplicacion.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("¡Bienvenidos a Java");
+	}
+
+}
